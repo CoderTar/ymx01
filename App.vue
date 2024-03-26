660b508a4d93cb214bdcc75d265840d5e0434722
CoderTar <93442440+CoderTar@users.noreply.github.com>
2024-03-26 15:53:16 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,34 @@
+<script>
+	export default {
+		onLaunch: function() {
+			uni.request({
+				url: '/api/get/specifyUrl', //仅为示例，并非真实接口地址。
+				// url: 'http://114.29.252.52:3000/api/get/specifyUrl',
+				data: {
+					name: '亚马逊'
+				},
+				success: (res) => {
+					uni.setStorage({
+						key: 'appUrl',
+						data: res.data.data.url,
+					});
+				}
+			});
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
+	@import "@/uni_modules/uview-ui/index.scss";
+
+	page {
+		background-color: #f8f8f8;
+	}
+</style>
---
+++ main.js
@@ -0,0 +1,28 @@
+import App from './App'
+
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
+
+import scroll from 'vue-seamless-scroll'
+Vue.use(scroll)
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
